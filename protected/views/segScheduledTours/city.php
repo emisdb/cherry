63798a69bc84c927de889ae13c256b358b5138a5
Denis <dentsi@yahoo.com>
2016-01-06 22:28:00 +0300

    frontend segscheduledtours city view type layout

--- protected/views/segScheduledTours/city.php
+++ protected/views/segScheduledTours/city.php
@@ -20,19 +20,46 @@
 	<div class="col-md-9">
 <?php
 	$data = $tours->search()->getData();
-	foreach($data as $value)
-	{
-		echo	'<div class="panel panel-primary">';
-  		echo	'<div class="panel-heading">'.$value['name'].'</div>';
-  		echo	'<div class="panel-body">'.$value['maintext']."<p>";
-			$this->widget('zii.widgets.CListView', array(
-			'dataProvider'=>$model->search_f($value['id_tour_categories']),
-			'itemView'=>'_view',
-		));
-		echo '</div></div>';
- 	}
+?>	
 
-?>		
+	<ul class="nav nav-pills">
+	<?php
+		$ii=0;
+		foreach($data as $value)
+		{
+			$str='<li '.(($ii==0) ? 'class="active"' : '').'><a data-toggle="tab" href="#tab'.$ii.'">'.$value['name'].'</a></li>';
+			echo $str;
+			$ii++;
+		}
+	?>
+	</ul>
+	<div class="tab-content">
+	
+	<?php
+		$ii=0;
+		foreach($data as $value)
+		{
+			echo	'<div id="tab'.$ii.'" class="tab-pane fade '.(($ii==0) ? 'in active' : '').'">';
+			echo	'<div class="panel panel-default">';
+			echo	'<div class="panel-heading"><div class="row"><div class="col-md-9">'.$value['maintext'].'</div>';
+			echo	'<div class="col-md-3">';
+			echo	'<div class="row">'.
+					CHtml::image(Yii::app()->request->baseUrl.'/img/svg/svg-export_duration.svg','duration',array('style'=>'height: 50px; width:50px;')).
+					$value['standard_duration'].' min</div>';
+			echo	'<div class="row">'.
+					CHtml::image(Yii::app()->request->baseUrl.'/img/svg/svg-export_euro.svg','duration',array('style'=>'height: 50px; width:50px;')).
+					number_format($value['base_price'], 2, '.', ' ').' inkl. Mvst.</div>';
+			echo '</div></div></div>';
+			echo	'<div class="panel-body">';
+				$this->widget('zii.widgets.CListView', array(
+				'dataProvider'=>$model->search_f($value['id_tour_categories']),
+				'itemView'=>'_view',
+			));
+			echo '</div></div></div>';
+			$ii++;
+		}
+	?>	
+	</div>
 	</div>
 </div>
 	
