d010fa22abc5f274a337b88ab460c19368be2881
Denis <dentsi@yahoo.com>
2016-01-21 12:49:46 +0300

    front page trimming

--- protected/views/segScheduledTours/front.php
+++ protected/views/segScheduledTours/front.php
@@ -7,10 +7,10 @@
 //		echo	CHtml::image(Yii::app()->request->baseUrl.'/img/top.jpg','info',array('style'=>'width: 100%;'));
 	?>
 	<!--<div class="gradient"><div>-->
-	<div style="width:100%;  text-align: center;" ><?php	echo	CHtml::image(Yii::app()->request->baseUrl.'/img/cherrytours_icon_white_rgb.png','info',array('style'=>'height: 50px; ')); ?></div>
+	<div style="width:100%;  text-align: center;" ><?php	echo	CHtml::image(Yii::app()->request->baseUrl.'/img/cherrytours_icon_white_rgb.png','info',array('style'=>'height: 35px; ')); ?></div>
 	<div class="frontlabel" >CHERRYTOURS</div>
 	<div class="frontlabel" style="font-size:2em; margin-top:150px;" >TOUREN, DIE MAN NICHT VERGISST</div>
-	<div class="frontlabel" style="font-size:1.2em;" >Tauchen Sie ein in Deutschlands gr&ouml;&szlig;te Metropolen</div>
+	<div class="frontlabel" style="font-size:1.3em; text-transform: none;   letter-spacing: 3px;" >Tauchen Sie ein in Deutschlands gr&ouml;&szlig;te Metropolen</div>
 	<div style="width:100%;  text-align: center; margin:30px 0 150px;" >	   <?php       $this->renderPartial('_select_front', array('model'=>$model)) ;?>
 </div>
 
@@ -20,17 +20,17 @@
 		<ul class="teaserhold">
 			<li><?php echo CHtml::link('<div class="teaser berlin"><div>'
 					.CHtml::image(Yii::app()->request->baseUrl.'/img/icon-museum.png','info')
-					.'</div><div class="frontlabel">CLASSIC BERLIN</div></div>',array('/berlin'));	
+					.'</div><div class="frontlabel">CLASSIC<p>BERLIN</div></div>',array('/berlin'));	
 			?>
 			</li>
 			<li><?php echo CHtml::link('<div class="teaser munchen"><div>'
 					.CHtml::image(Yii::app()->request->baseUrl.'/img/icon-castle.png','info',array('style'=>'height: 40px; margin-top:60px; '))
-					.'</div><div class="frontlabel">CLASSIC M&Uuml;NCHEN</div></div>',array('/munchen'));	
+					.'</div><div class="frontlabel">CLASSIC<p> M&Uuml;NCHEN</div></div>',array('/munchen'));	
 			?>
 			</li>
 			<li><?php echo CHtml::link('<div class="teaser hamburg"><div>'
 					.CHtml::image(Yii::app()->request->baseUrl.'/img/icon-food.png','info',array('style'=>'height: 40px; margin-top:60px; '))
-					.'</div><div class="frontlabel">HISTORICAL M&Uuml;NCHEN</div></div>',array('/munchen'));	
+					.'</div><div class="frontlabel">HISTORICAL<p> M&Uuml;NCHEN</div></div>',array('/munchen'));	
 			?>
 			</li>
 		</ul>
