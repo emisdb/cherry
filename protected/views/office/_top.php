b6616f0d09222575c63ca5239438596c11a9d444
Denis <dentsi@yahoo.com>
2016-02-19 23:45:38 +0300

    guide translation 5

--- protected/views/office/_top.php
+++ protected/views/office/_top.php
@@ -58,13 +58,13 @@
               <!-- User Account: style can be found in dropdown.less -->
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <img src="<?php echo Yii::app()->request->baseUrl."/img/str2/logo2.png" ?>" class="user-image" alt="User Image">
+                  <img src="<?php echo Yii::app()->request->baseUrl."/img/cherrytours_icon_colour_rgb.jpg" ?>" class="user-image" alt="User Image">
                   <span class="hidden-xs"><?php echo $info['guide']->firstname." ".$info['guide']->surname ?></span>
                 </a>
                 <ul class="dropdown-menu">
                   <!-- User image -->
                   <li class="user-header">
-                    <img src="<?php echo Yii::app()->request->baseUrl."/img/str2/logo2.png" ?>" class="img-circle" alt="User Image">
+                    <img src="<?php echo Yii::app()->request->baseUrl."/img/cherrytours_icon_colour_rgb.jpg" ?>" class="img-circle" alt="User Image">
                     <p>
                       <?php echo $info['guide']->firstname." ".$info['guide']->surname ?>
                       <small><?php echo $info['guide']->email ?></small>
